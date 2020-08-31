c9148ac74d9f0def9010edabcd7cb578b63666e7
PeiKuo <55939462+psh09018@users.noreply.github.com>
2020-08-31 15:40:21 +0800

    First Upload

---
+++ practice6-1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace practice6_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.board = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.autoboard = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.autoboard)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // board
+            // 
+            this.board.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.board.Location = new System.Drawing.Point(175, 455);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(150, 30);
+            this.board.TabIndex = 0;
+            this.board.TabStop = false;
+            this.board.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BoardMove);
+            this.board.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BoardMoving);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // autoboard
+            // 
+            this.autoboard.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.autoboard.Location = new System.Drawing.Point(175, 248);
+            this.autoboard.Name = "autoboard";
+            this.autoboard.Size = new System.Drawing.Size(150, 30);
+            this.autoboard.TabIndex = 1;
+            this.autoboard.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(151, 148);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(174, 202);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(24, 109);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(73, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "快速版";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(24, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(73, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "中速版";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(24, 58);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "慢速版";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 503);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.autoboard);
+            this.Controls.Add(this.board);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.board)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.autoboard)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox board;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox autoboard;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ practice6-1/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice6_1
+{
+    public partial class Form1 : Form
+    {
+        bool start = false;
+        bool but_st = false;
+        int boardnum = 25;
+        public Form1()
+        {
+            InitializeComponent();
+            reset();
+        }
+
+        PictureBox ball = new PictureBox();
+
+        int mouseX;
+
+        private void BoardMove(object sender, MouseEventArgs e)
+        {
+            mouseX = e.X;    
+        }
+
+        private void BoardMoving(object sender, MouseEventArgs e)
+        {
+            if (but_st)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    int x = board.Left + (e.X - mouseX);
+                    if (x < 0) { x = 0; }
+                    if (x > this.ClientSize.Width - board.Width)
+                    {
+                        x = this.ClientSize.Width - board.Width;
+                    }
+                    board.Left = x;
+                }
+            }
+        }
+
+        Label[] bricks = new Label[25];
+        private void GenerateBricks() {
+            int index = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    bricks[index] = new Label();
+                    bricks[index].Width = 100;
+                    bricks[index].Height = 35;
+                    bricks[index].Location = new Point(i*100, j*35);
+                    bricks[index].BackColor = Color.DarkOrange;
+                    bricks[index].BorderStyle = BorderStyle.Fixed3D;
+                    this.Controls.Add(bricks[index]);
+                    index++;
+                }
+            }
+        }
+
+        private void GenerateBall() {
+            ball = new PictureBox();
+            ball.Width = 32;
+            ball.Height = 32;
+            ball.Image = new Bitmap(Properties.Resources.pp,ball.Size);
+            ball.Location = new Point(180, 432);
+            this.Controls.Add(ball);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (board.Left != 180)
+            {
+                start = true;
+            }
+            if (start)
+            {
+                move();
+                AutoBoardMove();
+                CollidesAutoBoard();
+                collidesBricks();
+                CollidesBoard();
+            }
+        }
+
+        private void AutoBoardMove() {
+            if (ball.Top>=autoboard.Top)
+            {
+                if (Vx > 0 && autoboard.Right < 516)
+                    autoboard.Left += 5;
+                else if (Vx < 0 && autoboard.Left >= 0)
+                    autoboard.Left -= 5;
+            }
+            else {
+                if (Vx > 0 && autoboard.Left >= 0)
+                    autoboard.Left -= 5;
+                else if (Vx < 0 && autoboard.Right < 516)
+                    autoboard.Left += 5;
+            }
+        }
+
+        private void CollidesAutoBoard() {
+            if (ball.Top <= autoboard.Bottom && ball.Top > autoboard.Top && ball.Left +16> autoboard.Left && ball.Left+16 < autoboard.Right)
+            {
+                Vy = -Vy;
+            }
+            else if (ball.Bottom>= autoboard.Top && ball.Bottom < autoboard.Bottom && ball.Left+16 > autoboard.Left && ball.Left +16 < autoboard.Right)
+            {
+                Vy = -Vy;
+            }
+            else if (ball.Right >= autoboard.Left && ball.Left+16<autoboard.Left&&ball.Right < autoboard.Right && ball.Top + 16 > autoboard.Top && ball.Top + 16 < autoboard.Bottom) {
+                Vx = -Vx;
+            }
+            else if (ball.Left <= autoboard.Right && ball.Right-16>autoboard.Right&&ball.Left > autoboard.Left && ball.Top + 16 > autoboard.Top && ball.Top + 16 < autoboard.Bottom)
+            {
+                Vx = -Vx;
+            }
+        }
+
+        private void CollidesBoard() {
+            //if (ball.Bottom >= board.Top && ball.Left + 16 >= board.Left && ball.Right - 16 <= board.Right) {
+            if (ball.Bottom >= board.Top && ball.Left + 16 >= board.Left && ball.Right - 16 <= board.Right)
+            {
+                if (ball.Left + 16 < board.Left + board.Width/2)
+                {
+                    Vy = -Vy;
+                    Vx = -Math.Abs(Vx);
+                }
+                else if (ball.Left + 16 >= board.Width / 2) {
+                    Vy = -Vy;
+                    Vx = Math.Abs(Vx);
+                }
+            }
+        }
+
+        private void collidesBricks() {
+            for (int i = 0; i < 25; i++) {
+                if (ball.Top <= bricks[i].Bottom && ball.Top > bricks[i].Top && ball.Right -16< bricks[i].Right && ball.Left+16 > bricks[i].Left && !bricks[i].IsDisposed)
+                {
+                    bricks[i].Dispose();
+                    boardnum--;
+                    Vy = -Vy;
+                    break;
+                }
+                else if (ball.Bottom < bricks[i].Bottom && ball.Bottom >= bricks[i].Top && ball.Right -16< bricks[i].Right && ball.Left  +16> bricks[i].Left && !bricks[i].IsDisposed)
+                {
+                    bricks[i].Dispose();
+                    boardnum--;
+                    Vy = -Vy;
+                    break;
+                }
+                else if (ball.Right >= bricks[i].Left && ball.Right < bricks[i].Right && ball.Top + 16 > bricks[i].Top && ball.Bottom - 16 < bricks[i].Bottom && !bricks[i].IsDisposed)
+                {
+                    bricks[i].Dispose();
+                    boardnum--;
+                    Vx = -Vx;
+                    break;
+                }
+                else if (ball.Left <= bricks[i].Right && ball.Left > bricks[i].Left && ball.Top + 16 > bricks[i].Top && ball.Bottom - 16 < bricks[i].Bottom && !bricks[i].IsDisposed) {
+                    bricks[i].Dispose();
+                    boardnum--;
+                    Vx = -Vx;
+                    break;
+                }
+                if (boardnum == 0)
+                {
+                    timer1.Stop();
+                    start = false;
+                    boardnum = 25;
+                    MessageBox.Show("You win the Game!");
+                    MessageBox.Show("Press to Restart!");
+                    ball.Dispose();
+                    for (int j = 0; j < 25; j++)
+                    {
+                        if (!bricks[j].IsDisposed)
+                            bricks[j].Dispose();
+                    }
+                    reset();
+                }
+            }
+        }
+
+        int Vx = 0, Vy = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                Vx = 8;
+                Vy = 8;
+                board.Width = 200;
+                autoboard.Width = 70;
+            }
+            if (radioButton2.Checked)
+            {
+                Vx =10;
+                Vy = 10;
+            }
+            if (radioButton3.Checked)
+            {
+                Vx = 14;
+                Vy = 14;
+            }
+            //start = true;
+            but_st = true;
+            panel1.Hide();
+            radioButton2.Hide();
+            radioButton1.Hide();
+            radioButton3.Hide();
+            button1.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void move() {
+            ball.Top += Vy;
+            ball.Left += Vx;
+            if (ball.Left > ClientSize.Width-32)
+            {
+                Vx = -Vx;
+                ball.Left = ClientSize.Width-32;
+            }
+            else if (ball.Top > ClientSize.Height-32)
+            {
+                timer1.Stop();
+                MessageBox.Show("Game Over.");
+                MessageBox.Show("Press to Restart.");
+                ball.Dispose();
+                for (int i = 0; i < 25; i++) {
+                    if(!bricks[i].IsDisposed)
+                        bricks[i].Dispose();
+                }
+                reset();
+            }
+            else if (ball.Left <= 0)
+            {
+                Vx = -Vx;
+                ball.Left = 0;
+            }
+            else if (ball.Top<=0) {
+                Vy = -Vy;
+                ball.Top =0;
+            }
+        }
+
+        private void reset() {
+            start = false;
+            but_st = false;
+            GenerateBricks();
+            GenerateBall();
+            Width = 516;
+            Height = 550;
+            panel1.Width = 150;
+            panel1.Height = 200;
+            panel1.Top = 100;
+            panel1.Left = 176;
+            board.Width = 150;
+            board.Height = 25;
+            autoboard.Width = 100;
+            autoboard.Height = 30;
+            autoboard.Top = 200;
+            autoboard.Left = 176;
+            board.Top = 465;
+            board.Left = 180;
+            boardnum = 25;
+            button1.Show();
+            radioButton1.Show();
+            radioButton2.Show();
+            radioButton3.Show();
+            panel1.Show();
+            timer1.Interval = 30;
+            timer1.Start();
+        }
+    }
+}
